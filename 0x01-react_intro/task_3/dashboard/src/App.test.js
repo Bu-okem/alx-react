2c6a51fa8e0d0cf6eaeb8f79a2c915577ea0c8ff
bu-okem <chiobiegbulem@gmail.com>
2024-09-03 14:57:03 +0100

    task 3

---
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -0,0 +1,18 @@
+import { shallow } from 'enzyme';
+import App from './App';
+
+const wrapper = shallow(<App />);
+
+it('renders without crashing', () => {
+  shallow(<App />);
+});
+
+it('renders App-header', () => {
+  expect(wrapper.find('header.app-header').exists()).toEqual(true);
+});
+it('renders App-body', () => {
+  expect(wrapper.find('main.app-body').exists()).toEqual(true);
+});
+it('renders App-footer', () => {
+  expect(wrapper.find('footer.app-footer').exists()).toEqual(true);
+});
